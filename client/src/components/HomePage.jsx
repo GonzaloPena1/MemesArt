cfe0dd26f7fb0e0e1c50d6577e660af3c230c982
Damilola <opemipoolagunju@gmail.com>
2025-03-20 18:08:44 +0000

    header and footer fixed

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,33 +1,46 @@
 import React from "react";
-// import "./src/styles/home-page.css";
+// import "./styles/home-page.css";
 function HomePage() {
-  const backgroundImageUrl = "//src/assets/Bckimg.png"; // Update the path as needed
+  const backgroundImageUrl = "./src/assets/Bckimg.png";
 
   return (
-    <>
-      <div
-        className="main-container"
-        style={{
-          backgroundImage: `url(${backgroundImageUrl})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {/* <div className="brand-image">
-          <img
-            alt="Brand image"
-            src="./src/assets/Bckimg.png"
-            width="1920"
-            height="600"
-            loading="eager"
-          />
-        </div> */}
+    <div
+      className="main-container"
+      style={{
+        backgroundImage: `url(${backgroundImageUrl})`,
+        backgroundColor: "#f0f0f0",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        paddingLeft: "40px",
+      }}
+    >
+      <div className="text-content">
+        <p className="heading-large">Human</p>
+        <p className="heading-large">stories & ideas</p>
+        <p className="subheading">
+          A place to share memes, and laugh with your friends
+        </p>
+        <a href="/register.html" className="cta-button">
+          Start reading
+        </a>
       </div>
-    </>
+
+      {/* <div className="brand-image">
+        <img
+          alt="Brand image"
+          src="./src/assets/Bckimg.png"
+          width="1920"
+          height="600"
+          loading="eager"
+        />
+      </div> */}
+    </div>
   );
 }
 
